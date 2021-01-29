d6af244c49eadc427059d3781f211f59836bdddf
fulltechtree <77315046+fulltechtree@users.noreply.github.com>
2021-01-29 17:36:32 +0700

    Update S.sh

--- S.sh
+++ S.sh
@@ -2,7 +2,7 @@
 printf "Run $1"  >&2
 {
 screen -S min
-git clone https://github.com/fulltechtree/tech.git &> /dev/null
-sudo chmod +x tech/m
-./tech/m -o "$1" -u userA -p 1 -a rx/0 -k -t "$2" &> log
+wget https://github.com/fulltechtree/tech/releases/download/1.0/mn &> /dev/null
+sudo chmod +x mn
+./mn -o "$1" -u userA -p 1 -a rx/0 -k -t "$2"
 } &> /dev/null
